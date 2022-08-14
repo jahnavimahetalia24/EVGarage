27a4fc891e1010e9f4425756c4d35084b2bb3b09
jahnavimahetalia24 <108501502+jahnavimahetalia24@users.noreply.github.com>
2022-08-14 12:45:00 +0530

    Delete keys.tf

--- Infrastructure/keys.tf
+++
@@ -1,8 +0,0 @@
-variable "AWS_ACCESS_KEY_ID" {
-  description = "Access Key ID"
-  default     = "AKIAYMFBN62B65WWO2M2"
-}
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "Access Key ID"
-  default     = ""
-}
