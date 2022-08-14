5e478b6410dbc03ff78ba2d94f87dc26549c8c68
Jahnavi Mahetalia <jahnavimahetalia@Jahnavis-MacBook-Air.local>
2022-08-14 15:37:19 +0530

    adding files

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,15 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
+provider "aws" {
+  region = "us-east-1"
+  alias = "east"
 }
 
 data "aws_ami" "i" {
+  provider = aws.east
   owners = ["amazon"]
   most_recent = true
 
